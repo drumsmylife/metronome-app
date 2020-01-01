f40a48772684c10b8acdf40bed2a1f2c6c853335
Tommy Cervenka <tommy@cervenka.org>
2020-01-01 15:12:47 -0600

    added in audio files in metronome js

--- metronome-app/src/Metronome.js
+++ metronome-app/src/Metronome.js
@@ -1,9 +1,12 @@
 import React, { Component } from 'react';
 import './Metronome.css';
-
+import click1 from './click1.wav';
+import click2 from './click2.wav';
 class Metronome extends Component {
     constructor(props) {
       super(props);
+      this.click1 = new Audio(click1);
+      this.click2 = new Audio(click2);
   
       this.state = {
         playing: false,
@@ -17,6 +20,7 @@ class Metronome extends Component {
         const bpm = event.target.value;
         this.setState({ bpm });
       }
+      
     render() {
       const { playing, bpm } = this.state;
   
