4c097b2ab584cba8eb1d74a8b5ebdcd639f36a4a
Tommy Cervenka <tommy@cervenka.org>
2020-01-07 17:03:02 -0600

    got timer to start working and play actual beat and added playClick function in order for it to be called properly to work

--- metronome-app/src/Metronome.js
+++ metronome-app/src/Metronome.js
@@ -21,8 +21,44 @@ class Metronome extends Component {
         this.setState({ bpm });
       }
       startStop = () => {
-        this.click1.play();
-      }     
+        if (this.state.playing) {
+          // Stop the timer
+          clearInterval(this.timer);
+          this.setState({
+            playing: false
+          });
+        } else {
+          // Start a timer with the current BPM
+          this.timer = setInterval(
+            this.playClick,
+            (60 / this.state.bpm) * 1000
+          );
+          this.setState(
+            {
+              count: 0,
+              playing: true
+              // Play a click "immediately" (after setState finishes)
+            },
+            this.playClick
+          );
+        }
+      };
+
+      playClick = () => {
+        const { count, beatsPerMeasure } = this.state;
+      
+        // The first beat will have a different sound than the others
+        if (count % beatsPerMeasure === 0) {
+          this.click2.play();
+        } else {
+          this.click1.play();
+        }
+      
+        // Keep track of which beat we're on
+        this.setState(state => ({
+          count: (state.count + 1) % state.beatsPerMeasure
+        }));
+      };
     render() {
       const { playing, bpm } = this.state;
   
